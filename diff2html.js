a084bff88d138685c68063c2007aa0c63725475e
James Tomson <james.tomson@blackboard.com>
2011-06-29 15:59:27 -0400

    grinding on messages, import showdown.js from github flavored markdown

--- diff2html.js
+++ diff2html.js
@@ -1,11 +1,13 @@
 require.paths.unshift('/usr/local/lib/node/'); // bad brew install
 
 var diffHighlighter = require('./diffHighlighter');
+var showdown = require('./showdown.js');
 var fs = require('fs');
 var jsdom = require('jsdom');
 var sys = require('sys');
 
-// TODO Construct HTML preamble
+// Construct HTML preamble
+var output = '<!DOCTYPE html><html><head><link rel="stylesheet" type="text/css" href="diff.css" /></head><body>'
 
 // TODO Get commit details
 
@@ -16,26 +18,37 @@ var input = fs.readFileSync('/dev/stdin').toString();
 var diffHtml = diffHighlighter.diff2html(input);
 
 // Get notes for commit
-var notes = [{file: 1, diffline: 2, message: 'I will gladly pay you tuesday for this hamburger today'}];
+var notes = [{file: 1, diffline: 2, message: 'I will gladly pay you tuesday for this *hamburger* today\n -James'}];
 
 // TODO Get html from notes (markdown) ?
 
 // Place notes in diff html
+var composeNoteHtml = function(message) {
+    return '<tr class="noteRow"><td colspan="2">Note: </td><td class="noteMessage">' + showdown.makeHtml(message) + '</td></tr>'
+};
+
 jsdom.env(diffHtml,
           [
             './jquery-1.5.min.js'
           ],
-          function(errors, window) {
+          function(errors, w) {
               for (var idx in notes) {
                   var note = notes[idx];
-                  var diffRow = window.$("#file_index_" + note.file + " tr")[note.diffline];
+                  if (note.file < 0) {
+                     // comment is on the whole commit?
+                     continue; 
+                  }
+                  var diffRow = w.$("#file_index_" + note.file + " tr")[note.diffline];
                   if (diffRow == undefined) {
                       console.log('Could not find diffrow for note: ' + sys.inspect(note));
                       continue;
                   }
-                  window.$(diffRow).after('<tr><td colspan=3>' + note.message + '</td></tr>').appendTo(html);
-                  console.log(window.$('html').html());
+                  var noteHtml = composeNoteHtml(note.message);
+                  w.$(diffRow).after(noteHtml);
               }
+              output += w.$('body').html();
+              output += '</body></html>';
+              console.log(output);
           });
 
 // TODO end HTML
