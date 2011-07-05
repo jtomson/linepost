33799c8fbf726fb5825832e7139e5847b3c1dd35
Your Name <james.b.tomson@gmail.com>
2011-07-04 20:37:17 -0400

    split api and content routing

--- lps.js
+++ lps.js
@@ -13,7 +13,28 @@ var _repos = {};
 _repos['linepost'] = '/Users/james/sandbox/linepost';
 // ----------------
 
-var _getCommitInfo = function(repo_dir_path, sha, res) {
+var _api_sendError = function(error_code, error_msg, res) {
+    var error_stringified = JSON.stringify({'error': error_msg});
+    
+    res.writeHead(error_code,
+                  {'Content-Type': 'application/json',
+                   'Content-Length': error_stringified.length});
+    res.write(error_stringified);
+    res.end();
+    console.log('sent api error code: ' + error_code + ', error: ' + error_stringified );
+}
+
+// TODO - nicer 404 pages etc
+var _content_sendError = function(error_code, error_msg, res) {
+    res.writeHead(error_code,
+                  {'Content-Type': 'text/plain',
+                   'Content-Length': error_msg.length});
+    res.write(error_msg);
+    res.end();
+    console.log('sent content error code: ' + error_code + ', error: ' + error_msg );
+}
+
+var _api_sendGitShow = function(repo_dir_path, sha, res) {
     // SHA \01
     // Author Name <Author Email>\01
     // Subject \n Body \01
@@ -24,34 +45,21 @@ var _getCommitInfo = function(repo_dir_path, sha, res) {
           {cwd: repo_dir_path},
           function(error, stdout, stderr) {
               if (error) {
-                  console.log('Error running git show: ' + error);
-                  res.writeHead(500,
-                                'Error running git show: ' + error,
-                                {'Content-Type': 'text/plain'});
+                  _api_sendError(500, 'Error running git show: ' + error, res);
               }
               else if (stderr) {
-                  console.log('Error running git show - has stderr: ' + stderr);
-                  res.writeHead(500,
-                                'Error running git show - has stderr: ' + stderr,
-                                {'Content-Type': 'text/plain'});
+                  _api_sendError(500, 'Error running git show - has stderr: ' + stderr, res);
               }
               else if (stdout.length == 0) {
-                  console.log('Error running git show - no output');
-                  res.writeHead(500,
-                                'Error running git show - no output',
-                                {'Content-Type': 'text/plain'});
+                  _api_sendError(500, 'Error running git show - no output');
               }
               else {
                   var split_output_array = stdout.split('\01');
                   
-                  if (split_output_array.length < 5) {
-                      console.log('Error running git show - bad output: ' + stdout);
-                      res.writeHead(500,
-                                   'Error running git show - bad output: ' + stdout,
-                                   {'Content-Type': 'text/plain'});
+                  if (split_output_array.length != 5) {
+                      _api_sendError(500, 'Error running git show - bad output: ' + stdout, res);
                   }
                   else {
-                      console.log('Good output from git-show');
                       var stringified_commit = JSON.stringify({
                             'sha': split_output_array[0],
                             'author-name-and-email': split_output_array[1],
@@ -62,17 +70,35 @@ var _getCommitInfo = function(repo_dir_path, sha, res) {
                       res.writeHead(200, {'Content-Length': stringified_commit.length,
                                           'Content-Type': 'application/json'});
                       res.write(stringified_commit);
+                      res.end();
+                      console.log('sent good output from git-show');
                   }
               }
-              
-              res.end();
           });
+}
+
+var _isGoodSha = function(sha) {
+    var sha_matches = sha.match(/(?:\d|[a-e]){6,40}/i);
+    return (sha_matches !== null && sha_matches.length == 1);
 }
 
 app.use(connect.logger());
 
-app.get('/:repo/:sha', function(req, res) {
-    _getCommitInfo(_repos[req.params.repo], req.params.sha, res);
+app.get('/api/git-show/:repo/:sha', function(req, res) {
+    var repo = req.params.repo;
+    var sha = req.params.sha;
+    
+    // Do we have this repo name mapped to a local dir?
+    if (_repos[repo] === undefined) {
+        // TODO - nicer 404
+        _api_sendError(404, 'Undefined repo: "' + repo + '"', res);
+    }
+    else if (_isGoodSha(sha)) {
+        _api_sendGitShow(_repos[req.params.repo], req.params.sha, res);
+    }
+    else {
+        _api_sendError(404, 'Bad sha: "' + sha + '"', res);
+    }
 });
 
 app.listen(3000);
