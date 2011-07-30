71166036cdb4db873af290dd386ee87603170b4f
James Tomson <james.b.tomson@gmail.com>
2011-07-30 18:46:31 -0400

    fixjsstyle run

--- lps.js
+++ lps.js
@@ -22,17 +22,17 @@ var express = require('express'),
 
 (function init() {
     var is_new_db = false;
-    
+
     try {
         fs.lstatSync(_settings.db_path);
     }
     catch (error) {
         is_new_db = true;
     }
-    
+
     _db = new sqlite3.Database(_settings.db_path, function(error) { if (error) { throw error; } });
     _db.serialize(); // put in serial mode
-    
+
     // calls are queued so we can start using the db even if we haven't completed open above
     if (is_new_db) {
         // FIXME - not optimized at all, just blatted for the time being
@@ -83,12 +83,12 @@ var _delete_comment = function(id, callback) {
     _db.run('UPDATE comments SET deleted=1 WHERE id=$id',
             { '$id': id },
             callback);
-}
+};
 
 // assume params are clean
 var _get_comments = function(repo_name, sha, callback) {
     // FIXME stores in memory so assuming not a whole lot of comments
-    _db.all( 'SELECT * from comments WHERE repo_name = $repo_name AND commit_sha LIKE $commit_sha || "%" AND deleted = 0',
+    _db.all('SELECT * from comments WHERE repo_name = $repo_name AND commit_sha LIKE $commit_sha || "%" AND deleted = 0',
              { '$repo_name': repo_name,
                '$commit_sha': sha },
              function(error, rows) {
@@ -96,6 +96,9 @@ var _get_comments = function(repo_name, sha, callback) {
              });
 };
 
+// var _send_comment_mail = function(comment) {
+
+// };
 
 // Testing
 /*
@@ -109,25 +112,25 @@ var _get_comments = function(repo_name, sha, callback) {
         'comment_text': '"Hey there guy"'
     },
     function(error) { if(error) { throw error; } });
-    
+
     //_get_comments('linepost', 'ae12', function(rows) {_log(res, sys.inspect(rows));});
 }());
 */
 
 var _log = function(req, msg) {
-    console.log( (req.socket && req.socket.remoteAddress)
-     + ' - - [' + (new Date).toUTCString() + '] ' + msg );
+    console.log((req.socket && req.socket.remoteAddress) +
+                ' - - [' + (new Date).toUTCString() + '] ' + msg);
 };
 
 var _api_sendError = function(error_code, error_msg, res) {
     var error_stringified = JSON.stringify({'error': error_msg});
-    
+
     res.writeHead(error_code,
                   {'Content-Type': 'application/json',
                    'Content-Length': error_stringified.length});
     res.write(error_stringified);
     res.end();
-    _log(res, 'sent api error code: ' + error_code + ', error: ' + error_stringified );
+    _log(res, 'sent api error code: ' + error_code + ', error: ' + error_stringified);
 };
 
 // TODO - nicer 404 pages etc
@@ -137,7 +140,7 @@ var _content_sendError = function(error_code, error_msg, res) {
                    'Content-Length': error_msg.length});
     res.write(error_msg);
     res.end();
-    _log(res, 'sent content error code: ' + error_code + ', error: ' + error_msg );
+    _log(res, 'sent content error code: ' + error_code + ', error: ' + error_msg);
 };
 
 var _get_git_show = function(repo_name, sha, res, callback) {
@@ -147,23 +150,23 @@ var _get_git_show = function(repo_name, sha, res, callback) {
     // Author Date \01
     // DIFF EOF
     var format_str = '--pretty=format:"%H\01%an <%ae>\01%s\n%b\01%at\01"';
-    exec( _settings.git_bin + ' show ' + format_str + ' ' + sha,
+    exec(_settings.git_bin + ' show ' + format_str + ' ' + sha,
           {cwd: _settings.repos[repo_name]},
           function(error, stdout, stderr) {
               if (error) {
-                  callback( {status: 500, message: 'Error running git show: ' + error}, null );
+                  callback({status: 500, message: 'Error running git show: ' + error}, null);
               }
               else if (stderr) {
-                  callback( {status: 500, message: 'Error running git show - has stderr: ' + stderr}, null );
+                  callback({status: 500, message: 'Error running git show - has stderr: ' + stderr}, null);
               }
               else if (stdout.length === 0) {
-                  callback( {status: 500, message: 'Error running git show - no output'}, null);
+                  callback({status: 500, message: 'Error running git show - no output'}, null);
               }
               else {
                   var split_output_array = stdout.split('\01');
-                  
+
                   if (split_output_array.length !== 5) {
-                      callback( {status: 500, message: 'Error running git show - bad output: ' + stdout}, null );
+                      callback({status: 500, message: 'Error running git show - bad output: ' + stdout}, null);
                   }
                   else {
                       var response = {
@@ -172,7 +175,7 @@ var _get_git_show = function(repo_name, sha, res, callback) {
                             'subject-and-body': split_output_array[2],
                             'author-date': split_output_array[3],
                             'diff': split_output_array[4]
-                      }
+                      };
                       callback(null, response);
                       _log(res, 'sent good output from git-show');
                   }
@@ -205,7 +208,7 @@ var _respond = {
         res.render('commit.jade', {
             locals: {
                 'sha': sha,
-                'repo': repo_name 
+                'repo': repo_name
             },
             layout: false
         });
@@ -213,13 +216,13 @@ var _respond = {
     'json': function(repo_name, sha, res) {
         // return the git-show results & comments in one response
         var git_show_and_comments = {};
-        
+
         _get_git_show(repo_name, sha, res, function(error, result) {
             if (error) {
                 _api_sendError(error.status, error.message, res);
                 return;
             }
-            
+
             git_show_and_comments.git_show = result;
 
             _get_comments(repo_name, sha, function(error, result) {
@@ -247,7 +250,7 @@ app.get('/:repo/:sha', function(req, res) {
     var repo_name = req.params.repo;
     var sha = req.params.sha;
     var format = req.query.format || 'html';
-    
+
     // Do we have this repo name mapped to a local dir?
     if (_settings.repos[repo_name] === undefined) {
         var msg = 'Undefined repo: "' + repo_name + '"';
@@ -259,7 +262,7 @@ app.get('/:repo/:sha', function(req, res) {
         }
         return;
     }
-    
+
     if (_isGoodSha(sha)) {
         try {
             _respond[format](repo_name, sha, res);
@@ -279,7 +282,7 @@ app.post('/:repo/:sha/comments', function(req, res) {
     var repo_name = req.params.repo;
     var sha = req.params.sha;
     _log(res, 'Received POST body: ' + sys.inspect(req.body));
-    
+
     // TODO - validate vals?
     var now = new Date().getTime();
     var comment = {
@@ -291,7 +294,7 @@ app.post('/:repo/:sha/comments', function(req, res) {
         added_timestamp: now,
         edited_timestamp: now
     };
-    
+
     _add_comment(comment, function(error) {
         if (error) {
             _api_sendError(500, error, res);
@@ -316,7 +319,7 @@ app.post('/:repo/:sha/comments', function(req, res) {
 // edit an existing comment
 app.put('/:repo/:sha/comments/:id', function(req, res) {
     _log(res, 'Received PUT body: ' + sys.inspect(req.body));
-    
+
     // TODO - validate?
     _update_comment(req.params.id, req.body.comment_text, function(error) {
         if (error) {
